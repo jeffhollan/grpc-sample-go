0b0bdc2671af149d756040bbadbc2f083e13e1f5
Jeff Hollan <jeffhollan@outlook.com>
2021-11-24 09:39:08 -0800

    adding secure gRPC support

--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 )
 
 require (
+	github.com/cncf/xds/go v0.0.0-20211011173535-cb28da3451f1 // indirect
+	github.com/envoyproxy/go-control-plane v0.9.10-0.20210907150352-cf90f659a021 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
--- https-frontend/main.go
+++ https-frontend/main.go
@@ -7,6 +7,8 @@ import (
 
 	pb "github.com/jeffhollan/grpc-sample-go/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials/xds"
 )
 
 func main() {
@@ -15,8 +17,14 @@ func main() {
 
 	if !ok {
 		addr = "localhost:50051"
-		opts = append(opts, grpc.WithInsecure())
 	}
+	creds, err := xds.NewClientCredentials(xds.ClientOptions{
+		FallbackCreds: insecure.NewCredentials(),
+	})
+	if err != nil {
+		log.Fatalf("error creating client credentials: %v", err)
+	}
+	opts = append(opts, grpc.WithTransportCredentials(creds))
 
 	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
